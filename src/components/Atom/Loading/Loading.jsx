d8302176bc20b76c840c35785b0c77beac99f708
AnaAssuncao <apoassuncao@gmail.com>
2021-07-08 21:57:25 -0300

    Criação componente Loading, implementação Wallet

---
+++ src/components/Atom/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loading.scss'
+
+const Loading = () => {
+  return (
+    <div className= "loading">
+      Carregando...
+    </div>
+  )
+}
+
+export default Loading

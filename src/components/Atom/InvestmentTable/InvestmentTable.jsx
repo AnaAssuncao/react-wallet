b898eed99e02265140f500b17bf7a8b136fd9d74
AnaAssuncao <apoassuncao@gmail.com>
2021-07-12 11:47:23 -0300

    Acréscimo Suspense

--- src/components/Atom/InvestmentTable/InvestmentTable.jsx
+++ src/components/Atom/InvestmentTable/InvestmentTable.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import ReactDataGrid from 'react-data-grid';
+import React,{Suspense} from 'react'
 import PropTypes from 'prop-types'
 
+import {Loading} from '../Loading'
 import './investmentTable.scss'
+
+const ReactDataGrid = React.lazy(() => import('react-data-grid'));
 
 const InvestmentTable = ({columns,rows}) =>{
      const heigthTable = ((rows.length+1)*35+5)
     return (   
         <div className="investmentTable">
-            <ReactDataGrid
-                style={{height: heigthTable + "px", maxHeight: "350px"}}
-                columns={columns}
-                rows={rows}
-                rowGetter={i => rows[i]}
-                rowsCount={rows.length}
-                className="rdg-light investmentTable__container"
-                />
+            <Suspense fallback={<Loading/>}>
+                <ReactDataGrid
+                        style={{height: heigthTable + "px", maxHeight: "350px"}}
+                        columns={columns}
+                        rows={rows}
+                        rowGetter={i => rows[i]}
+                        rowsCount={rows.length}
+                        className="rdg-light investmentTable__container"
+                        />
+            </Suspense>
         </div>
     )
 }

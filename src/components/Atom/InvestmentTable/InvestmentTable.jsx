dcbffbc20b124d0c69bed6ebe974476c8d6da70b
AnaAssuncao <apoassuncao@gmail.com>
2021-07-15 21:40:36 -0300

    Implementação tabela ag-grid-react

--- src/components/Atom/InvestmentTable/InvestmentTable.jsx
+++ src/components/Atom/InvestmentTable/InvestmentTable.jsx
@@ -1,24 +1,44 @@
-import React,{Suspense} from 'react'
+import React from 'react'
+import {Suspense} from 'react'
+import {AgGridReact} from 'ag-grid-react'
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-balham.css'
 import PropTypes from 'prop-types'
 
 import {Loading} from '../Loading'
 import './investmentTable.scss'
 
-const ReactDataGrid = React.lazy(() => import('react-data-grid'));
-
 const InvestmentTable = ({columns,rows}) =>{
-     const heigthTable = ((rows.length+1)*35+5)
+  const heigthTable ={
+    row:35,
+    header:25,
+    max:350,
+  }
+
+  const totalRows = ((rows.length)*heigthTable.row+heigthTable.header+1)
+  const heightSize = totalRows<heigthTable.max?totalRows:heigthTable.max
+
     return (   
-        <div className="investmentTable">
+        <div className="investmentTable" >
             <Suspense fallback={<Loading/>}>
-                <ReactDataGrid
-                        style={{height: heigthTable + "px", maxHeight: "350px"}}
-                        columns={columns}
-                        rows={rows}
-                        rowGetter={i => rows[i]}
-                        rowsCount={rows.length}
-                        className="rdg-light investmentTable__container"
-                        />
+            <div className="ag-theme-balham" style={{height:heightSize, width: "62rem"}}>
+              <AgGridReact
+                    defaultColDef={{
+                        flex: 1,
+                        minWidth: 100,
+                        sortable: true,
+                        resizable: true
+                    }} 
+                    accentedSort={true}
+                    sortingOrder={['desc', 'asc', null]}
+                    animateRows={true}
+                    headerHeight={heigthTable.header}
+                    rowHeight={heigthTable.row}
+                    rowData={rows}
+                    columnDefs={columns}
+                  >
+              </AgGridReact>
+            </div>
             </Suspense>
         </div>
     )

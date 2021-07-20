845484f0127c391a89b720ed65dda21acf64d5c4
AnaAssuncao <apoassuncao@gmail.com>
2021-07-19 22:10:58 -0300

    Alteração no dados de teste do Patrimônio

--- src/components/Organism/WalletEquity/getDataAssets.js
+++ src/components/Organism/WalletEquity/getDataAssets.js
@@ -1,43 +1,63 @@
-import assets from "../../../TestData/assets-DataTest.json"
-import tableAssets from "../../../TestData/tableAssets-DataTest.json"
+import assetsDataTest from "../../../TestData/tableAssets-DataTest"
+const assets = assetsDataTest
+modifyTest()
+
+function modifyTest(){
+    const amount = (Math.random() * 10000000)
+    assets.totalEquity.total.amount =  numberToCurrenty(amount)
+    assets.totalEquity.total.cost = numberToCurrenty(amount * 0.80)
+    assets.totalEquity.total.result = numberToCurrenty(amount * 0.20)
+
+    for(let i in assets){
+        if(assets[i].rows){
+            const amountAssets = amount*(assets[i].total.percentage/100)
+            assets[i]=addValueRows(assets[i],amountAssets)
+            assets[i].total.amount = numberToCurrenty(amountAssets)
+            assets[i].total.cost = numberToCurrenty(amountAssets*0.80)
+            assets[i].total.result = numberToCurrenty(amountAssets*0.20)
+        }
+    }
+}
+
+function addValueRows(infoAssets,amountAssets){
+    infoAssets.rows.forEach((data)=>{
+        const amount = amountAssets*(data.percentage/100)
+        const cost = amount*0.80
+        const result = amount*0.20
+    
+        data.cost = numberToCurrenty(cost)
+        data.result = numberToCurrenty(result)
+        data.amount = numberToCurrenty(amount)
+    })
+    return infoAssets
+}   
+
+function numberToCurrenty(number){
+    return number.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
 
 function getAllAssets(){
-    const data = assets
-    for(let nameAssets in data){
-        data[nameAssets].cost = numberToCurrenty(data[nameAssets].cost)
-        data[nameAssets].result = numberToCurrenty(data[nameAssets].result)
-        data[nameAssets].amount = numberToCurrenty(data[nameAssets].amount)
+    const data = {
+        totalEquity: assets.totalEquity.total,
+        directTreasure: assets.directTreasure.total,
+        stocks: assets.stocks.total,
+        realEstateFund: assets.realEstateFund.total,
     }
     return data
 }
 
 function getInfAssetsDirectTreasure(){
-    const tableData = tableAssets.directTreasure
-    tableData.rows.forEach((data)=>{ 
-        data.cost = numberToCurrenty(data.cost)
-        data.result= numberToCurrenty(data.result)
-        data.value= numberToCurrenty(data.value)
-    })
+    const tableData = assets.directTreasure
     return tableData
 }
 
 function getInfAssetsStocks(){
-    const tableData = tableAssets.stocks
-    tableData.rows.forEach((data)=>{ 
-        data.cost = numberToCurrenty(data.cost)
-        data.result= numberToCurrenty(data.result)
-        data.value= numberToCurrenty(data.value)
-    })
+    const tableData = assets.stocks
     return tableData
 }
 
 function getInfAssetsRealEstateFund(){
-    const tableData = tableAssets.realEstateFund
-    tableData.rows.forEach((data)=>{ 
-        data.cost = numberToCurrenty(data.cost)
-        data.result= numberToCurrenty(data.result)
-        data.value= numberToCurrenty(data.value)
-    })
+    const tableData = assets.realEstateFund
     return tableData
 }
 
@@ -46,8 +66,4 @@ export{
     getInfAssetsStocks,
     getInfAssetsDirectTreasure,
     getInfAssetsRealEstateFund
-}
-
-function numberToCurrenty(number){
-    return number.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 }

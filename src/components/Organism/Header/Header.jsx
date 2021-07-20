8de0b7e3ac51cba06e3bf6bb1726c056f77a8cd5
AnaAssuncao <apoassuncao@gmail.com>
2021-07-20 16:13:11 -0300

    Icones header

--- src/components/Organism/Header/Header.jsx
+++ src/components/Organism/Header/Header.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react"
 
-import {ContainerMenuHeader} from '../../Molecule/ContainerMenuHeader'
-import {Logo} from '../../Atom/Logo'
+import {ContainerMenuHeader} from "../../Molecule/ContainerMenuHeader"
+import {Logo} from "../../Atom/Logo"
 
-import './header.scss'
+import walletIcon from "../../../img/wallet_icon.svg"
+import transactionsIcon from "../../../img/transactions_icon.svg"
+import calcIcon from "../../../img/calc_icon.svg"
+import newsIcon from "../../../img/news_icon.svg"
+import chartIcon from "../../../img/chart_icon.svg"
+import configIcon from "../../../img/config_icon.svg"
+
+import "./header.scss"
 const Header = () =>{
     const arrayListMenu =[
-        {name:"CARTEIRAS",value:"wallet", srcImg:"https://uploads-ssl.webflow.com/5e70e7ceff01278b0a221b74/60e9d7d19eacf8582aae84c6_wallet_icon2.svg", extraClass:"itemMenuHeader__fist"},
-        {name:"NEGOCIAÇÕES",value:"negotiations", srcImg:"https://uploads-ssl.webflow.com/5e70e7ceff01278b0a221b74/60e9d92402e5226b099abc2a_transactions.svg", extraClass:""},
-        {name:"INFO. TRIBUTOS",value:"taxes", srcImg:"https://uploads-ssl.webflow.com/5e70e7ceff01278b0a221b74/60e9d9b44b8a526e823b23d5_Taxs.png", extraClass:""},
-        {name:"NOTÍCIAS",value:"news", srcImg:"https://uploads-ssl.webflow.com/5e70e7ceff01278b0a221b74/60e9dbb5c8ec34246ea4cd62_news2.svg", extraClass:""},
-        {name:"INDICADORES",value:"indicators", srcImg:"https://uploads-ssl.webflow.com/5e70e7ceff01278b0a221b74/60e9df510ad554abc07fdf69_chart.svg", extraClass:""}]        
+        {name:"CARTEIRAS",value:"wallet", srcImg:walletIcon, extraClass:"itemMenuHeader__fist"},
+        {name:"NEGOCIAÇÕES",value:"negotiations", srcImg:transactionsIcon, extraClass:""},
+        {name:"INFO. TRIBUTOS",value:"taxes", srcImg:calcIcon, extraClass:""},
+        {name:"NOTÍCIAS",value:"news", srcImg:newsIcon, extraClass:""},
+        {name:"INDICADORES",value:"indicators", srcImg:chartIcon, extraClass:""}]        
         
-    const srcImgConfig="https://uploads-ssl.webflow.com/5e70e7ceff01278b0a221b74/60e9da3e4cbd09147cb53fc2_config.svg"
     return (
         <div className="header">
             <div className="header__logo">
@@ -24,7 +30,7 @@ const Header = () =>{
             </div>
             <div>
             <div className={"header__config"}> 
-                <img src={srcImgConfig} loading="lazy" alt="" className="itemMenuHeader__icon"></img>
+                <img src={configIcon} loading="lazy" alt="" className="itemMenuHeader__icon"></img>
                 <p className="itemMenuHeader__text">CONFIGURAÇÕES</p>
              </div>
             </div>

7c458027343c4febdb7624d5698e8e60a6f26aee
AnaAssuncao <apoassuncao@gmail.com>
2021-07-20 21:41:00 -0300

    Alteração nos argumentos

--- src/components/Atom/ItemMenuHeader/ItemMenuHeader.stories.js
+++ src/components/Atom/ItemMenuHeader/ItemMenuHeader.stories.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ItemMenuHeader from './ItemMenuHeader';
+import React from 'react'
+import ItemMenuHeader from './ItemMenuHeader'
+import transactionsIcon from "../../../img/transactions_icon.svg"
 
 //👇 This default export determines where your story goes in the story list
 export default {
   title: 'ATOM/ItemMenuHeader',
   component: ItemMenuHeader,
   argTypes: { onClick: { action: 'clicked' } }
-};
+}
 
 //👇 We create a “template” of how args map to rendering
-const Template = (args) => <ItemMenuHeader {...args} />;
+const Template = (args) => <ItemMenuHeader {...args} />
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 Primary.args = {
-  classSelect:"ItemMenuHeader__item__select", 
-  name:"Patrimônio",
-  value:"equity",
-  handleSelectNavigations:()=>{}
-};
+  name:"NEGOCIAÇÕES",
+  value:"negotiations", 
+  srcImg:transactionsIcon, 
+  extraClass:"",
+}

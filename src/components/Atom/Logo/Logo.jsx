a5f9e107a399f6ac9240bec99590e77ad624c09e
AnaAssuncao <apoassuncao@gmail.com>
2021-07-27 16:14:50 -0300

    Alterações no design do cabeçalho

--- src/components/Atom/Logo/Logo.jsx
+++ src/components/Atom/Logo/Logo.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './logo.scss'
 import textLogo from "../../../img/text_logo.svg"
 import walletLogo from "../../../img/wallet_logo.svg"
+import forYouLogo from "../../../img/4u.svg"
 
 const Logo = () => {
   return (
     <div className="logo">
-        <img src={walletLogo} alt="Logo" className="header__imgWallet"></img>
-        <img src={textLogo} alt="Logo" className="header__imgText"></img>
+        <img src={walletLogo} alt="Logo" className="logo__imgWallet"></img>
+        <img src={textLogo} alt="Logo" className="logo__imgText"></img>
+        <img src={forYouLogo} alt="Logo" className="logo__img4u"></img>
     </div>
   )
 }

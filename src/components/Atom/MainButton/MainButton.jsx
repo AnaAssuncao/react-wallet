da7fbae22b63b2ccab1ea3efdd5cb44ac93c0059
AnaAssuncao <apoassuncao@gmail.com>
2021-08-16 20:55:32 -0300

    feat: implementação do Material-UI no componente MainButton

--- src/components/Atom/MainButton/MainButton.jsx
+++ src/components/Atom/MainButton/MainButton.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './mainButton.scss'
+import React from "react";
+import { makeStyles , emphasize} from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types"
+import "./mainButton.scss"
 
-const MainButton = ({children,...others}) => {
+const colorsButton={
+  save:{
+    background:"#34df53",
+    color:"#fcfcfc"
+  },
+  cancel:{
+    background:"#1976d2",
+    color:"#fcfcfc"
+  },
+  delete:{
+    background:"#c70e50",
+    color:"#fcfcfc"
+  }
+}
 
-  return (
-    <button type="button" className= "main-button" {...others}>
-      {children}
-    </button>
-  )
+const MainButton = ({children,color, ...others}) => {
+  debugger
+  const useStyles = makeStyles({
+    root: {
+      background:colorsButton[color].background,
+      borderRadius: 3,
+      boxShadow: '0px 1px 3px #000',
+      color: colorsButton[color].color,
+    },
+    label:{
+      "&:hover": {
+        backgroundColor: emphasize(colorsButton[color].background, 0.2),
+      '&:active': {
+        boxShadow: 'none',
+        backgroundColor: emphasize(colorsButton[color].background, 0.9),
+      },
+      '&:focus': {
+        boxShadow: '0px 1px 6px -2 #000',
+      },
+  }}  
+  })
+  const classes = useStyles();
+  return <Button className={classes.root + " " + classes.label} {...others}>{children}</Button>;
 }
 
 MainButton.propTypes = {
   children: PropTypes.string,
+  color: PropTypes.string
 }
 
 MainButton.defaultProps = {

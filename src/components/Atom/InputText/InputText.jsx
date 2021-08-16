7839c395dd7917cfae08e7bf932996fa0d74bf77
AnaAssuncao <apoassuncao@gmail.com>
2021-08-16 18:21:22 -0300

    fix: atributos com Spread no componente

--- src/components/Atom/InputText/InputText.jsx
+++ src/components/Atom/InputText/InputText.jsx
@@ -2,14 +2,13 @@ import React from "react"
 import PropTypes from "prop-types"
 import "./inputText.scss"
 
-const InputText = ({nameInput,maxLength,handleInputText}) => {
+const InputText = ({nameInput,...others}) => {
   return (
     <div className="input-text">
       <p className="input-text__name">{nameInput}</p>
       <input type="text" 
-            maxLength={maxLength} 
             className="input-text__input"
-            onChance={handleInputText}/>
+            {...others}/>
     </div>
   )
 }

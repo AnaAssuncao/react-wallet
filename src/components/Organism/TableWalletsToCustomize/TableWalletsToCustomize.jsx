3632e83253739607039696c6dfca92f28c3202f7
AnaAssuncao <apoassuncao@gmail.com>
2021-08-17 18:16:20 -0300

    fix: alteração no cabeçalho da tabela e na estado dataTable

--- src/components/Organism/TableWalletsToCustomize/TableWalletsToCustomize.jsx
+++ src/components/Organism/TableWalletsToCustomize/TableWalletsToCustomize.jsx
@@ -7,58 +7,52 @@ import { MainButton } from "../../Atom/MainButton"
 import "./tableWalletsToCustomize.scss"
 
 const TableWalletsToCustomize = ({walletsToCustomize,handleAssetsChanges})=>{
+    const namesHeader = ["CATEGORIA","CÓDIGO ATIVO","DESCRIÇÃO ATIVO","% CARTEIRA ATUAL","% APOS A MUDANÇA"]
     const defaultWallets = Object.assign({}, walletsToCustomize)
-    const [dataTable, setDataTable]=useState({assets:defaultWallets, arrayChangesAssets:[]})
+    const [dataTable, setDataTable]=useState({assets:defaultWallets,changesAssets:{}})
     const keysDataTable = Object.keys(dataTable.assets)
     const total = getTotalAssets(dataTable,keysDataTable) 
-    // botão add vai estar neste componente
+
     const handleValuePercentage = (valuePercentage, codeWallet)=>{
         const percent = valuePercentage/100
-        debugger
         dataTable.assets[codeWallet].percentCustomed= percent
-        const infoWalletChanges={
+        dataTable.changesAssets[codeWallet]={   
             codeWallet: codeWallet,
             newInfo: percent
         }
-        dataTable.arrayChangesAssets.push(infoWalletChanges)
-        setDataTable({assets: dataTable.assets, arrayChangesAssets:dataTable.arrayChangesAssets})
-        handleAssetsChanges(dataTable.arrayChangesAssets)
+        setDataTable({assets: dataTable.assets, changesAssets:dataTable.changesAssets})
+        handleAssetsChanges(dataTable.changesAssets)
     }
 
     return(
-        <React.Fragment>
-            <section className="table-wallets-customize">
-                <div className="table-wallets-customize__header">
-                    <span className="table-wallets-customize__title">Ativos Da Carteira</span>
-                    <MainButton color={"save"} size="medium" variant="outlined">+ ADICIONAR NOVO ATIVO</MainButton>
-                </div>
-                <div className="table-wallets-customize__columns">
-                    <span className="table-wallets-customize__cell">CATEGORIA</span>
-                    <span className="table-wallets-customize__cell">CÓDIGO ATIVO</span>
-                    <span className="table-wallets-customize__cell">DESCRIÇÃO ATIVO</span>
-                    <span className="table-wallets-customize__cell">% CARTEIRA ATUAL</span>
-                    <span className="table-wallets-customize__cell">% APOS A MUDANÇA</span>  
-                </div>
-                {keysDataTable.map((codeAssets)=>
+        <section className="table-wallets-customize">
+            <div className="table-wallets-customize__header">
+                <span className="table-wallets-customize__title">Ativos Da Carteira</span>
+                <MainButton color={"save"} size="medium" variant="outlined">+ ADICIONAR NOVO ATIVO</MainButton>
+            </div>
+            <div className="table-wallets-customize__columns">
+                {namesHeader.map((name)=>{    
+                    return <span className="table-wallets-customize__cell" key={name}>{name}</span>
+                })}
+            </div>
+            {keysDataTable.map((codeAssets)=>
                     dataTable.assets[codeAssets].percentCustomed>=0 &&
                     <TableRowForView tableRowData={dataTable.assets[codeAssets]}
                                     handleValuePercentage={handleValuePercentage}
-                                    key={codeAssets}
-                                    />
-                )}
-                <div className="table-wallets-customize__footer">
-                    <div>
-                        <span className="table-wallets-customize__total">TOTAL GERAL</span>
-                        <span className="table-wallets-customize__value-total">{(total*100).toFixed()} %</span>
-                    </div>
-                    {total>1 &&
-                        <p className="table-wallets-customize__text-alert">Valor acima de 100%</p>}
-                    {total>0 && total<1 &&
-                        <p className="table-wallets-customize__text-alert">Complete até 100%</p>}
+                                key={codeAssets}/>            
+            )}
+            <div className="table-wallets-customize__footer">
+                <div>
+                    <span className="table-wallets-customize__total">TOTAL GERAL</span>
+                    <span className="table-wallets-customize__value-total">{(total*100).toFixed()} %</span>
                 </div>
-            </section>
-        </React.Fragment>
-    )   
+                {total>1 &&
+                    <p className="table-wallets-customize__text-alert">Valor acima de 100%</p>}
+                {total>0 && total<1 &&
+                    <p className="table-wallets-customize__text-alert">Complete até 100%</p>}
+            </div>
+        </section>
+    ) 
 }
 
 TableWalletsToCustomize.propTypes={

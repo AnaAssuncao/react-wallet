c912bf6e99d48d3c99e54e861a3103b4ab1113ba
AnaAssuncao <apoassuncao@gmail.com>
2021-08-18 16:32:53 -0300

    fix: uso do useState no componente TableRowForView

--- src/components/Molecule/TableRowForView/TableRowForView.jsx
+++ src/components/Molecule/TableRowForView/TableRowForView.jsx
@@ -1,22 +1,38 @@
-import React from "react"
+import {useState} from "react"
 import PropTypes from "prop-types"
 import {CounterPercentage} from "../../Atom/CounterPercentage"
 import {MainButton} from "../../Atom/MainButton"
 
 import "./tableRowForView.scss"
 
-const TableRowForView = ({tableRowData, handleValuePercentage}) => {
+const TableRowForView = ({tableRowData, handleValuePercentage, handleDeleteRow}) => {
+  const [percentage, setPercentage] = useState(tableRowData.percentWallet)
+
+  const handlePercentage = (valuePercentage)=>{
+    const value = valuePercentage/100
+    setPercentage(value)
+    handleValuePercentage(valuePercentage,tableRowData.code)
+  }
+
+  const handleDelete = ()=>{
+    const percent= 0
+    handleValuePercentage(percent,tableRowData.code)
+    handleDeleteRow(tableRowData.code)
+  }
+
   return (
     <div className="table-row-for-view">
       <span className="table-row-for-view__text">{tableRowData.nameCategory}</span>
       <span className="table-row-for-view__text" >{tableRowData.code}</span>
       <span className="table-row-for-view__text" >{tableRowData.name}</span>
       <span className="table-row-for-view__text" >{tableRowData.percentWallet*100}%</span>
-      <CounterPercentage valuePercent={tableRowData.percentCustomed}
+      <CounterPercentage valuePercent={percentage}
         colorButton={"dark"}
         maxLength={3}
-        handleValuePercentage={(valuePercentage)=>handleValuePercentage(valuePercentage,tableRowData.code)}/>
-       <MainButton color={"delete"} size="medium" onClick={()=>handleValuePercentage(-1,tableRowData.code)}>Deletar</MainButton>
+        style={{width:"10rem"}}
+        handleValuePercentage={(valuePercentage)=>handlePercentage(valuePercentage)}/>
+       <MainButton color={"delete"} size="medium" style={{marginLeft:"1rem"}} 
+                  onClick={()=>handleDelete()}>Deletar</MainButton>
     </div>
   )
 }

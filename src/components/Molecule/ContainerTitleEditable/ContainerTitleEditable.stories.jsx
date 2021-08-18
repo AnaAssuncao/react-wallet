36000729a418aeabf972cb27d63b47f971804fae
AnaAssuncao <apoassuncao@gmail.com>
2021-08-18 16:28:52 -0300

    fix: alterações para visualização no storybook

--- src/components/Molecule/ContainerTitleEditable/ContainerTitleEditable.stories.jsx
+++ src/components/Molecule/ContainerTitleEditable/ContainerTitleEditable.stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ContainerTitleEditable from './ContainerTitleEditable.stories'
+import ContainerTitleEditable from './ContainerTitleEditable'
 
 //👇 This default export determines where your story goes in the story list
 export default {
@@ -12,5 +12,13 @@ const Template = (args) => <ContainerTitleEditable {...args} />
 
 export const Primary = Template.bind({})
 Primary.args = {
- 
+  nameInput:"Nome da Carteira",
+  defaultValuesWallet:{    
+    totalEquity:{
+      category:"customWallets",
+      name:"Não Classificados",
+      percentEquity:0.80
+  }
+},
+  handleHeaderChanges:()=>{}
 }

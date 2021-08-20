160a9f6ebe6dcb446b67f7bc3f9f198e4085be23
AnaAssuncao <apoassuncao@gmail.com>
2021-08-19 22:30:20 -0300

    fix: alteração nos argumentos do storybook - TableWalletsToCustomize

--- src/components/Organism/TableWalletsToCustomize/TableWalletsToCustomize.stories.js
+++ src/components/Organism/TableWalletsToCustomize/TableWalletsToCustomize.stories.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React from "react"
 
-import TableWalletsToCustomize from "./TableWalletsToCustomize";
-
+import TableWalletsToCustomize from "./TableWalletsToCustomize"
+import {assets} from "../../../TestData/walletsCustomize-DataTest.json"
 //👇 This default export determines where your story goes in the story list
 export default {
   title: "ORGANISM/TableWalletsToCustomize",
   component: TableWalletsToCustomize
-};
+}
 
 //👇 We create a “template” of how args map to rendering
-const Template = (args) => <TableWalletsToCustomize {...args} />;
+const Template = (args) => <TableWalletsToCustomize {...args} />
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 Primary.args = {
-
-};
+  walletsToCustomize:assets,
+  handleAssetsChanges:()=>{},
+  handleTotalPercent:()=>{}
+}

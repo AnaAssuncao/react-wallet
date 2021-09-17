66e8fe69dfb98d99d8c10c779777084f6c62bd52
AnaAssuncao <apoassuncao@gmail.com>
2021-09-16 21:49:55 -0300

    fix: retirada do ;

--- src/components/Atom/AutocompleteInput/AutocompleteInput.jsx
+++ src/components/Atom/AutocompleteInput/AutocompleteInput.jsx
@@ -1,7 +1,7 @@
 import './autocompleteInput.scss'
 import {useState} from "react"
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField'
+import Autocomplete from '@material-ui/lab/Autocomplete'
 
 const AutocompleteInput = ({style,dataInput, nameLabel,value, handleAutocomplete}) => {
   const [inputValue, setInputValue] = useState("")
@@ -21,7 +21,7 @@ const AutocompleteInput = ({style,dataInput, nameLabel,value, handleAutocomplete
         renderInput={(params) => <TextField {...params} placeholder={nameLabel}/>}
         inputValue={inputValue}
         onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
+          setInputValue(newInputValue)
         }}
       />
   )
